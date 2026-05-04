fbed9bb3cb1198db73e220e1dd9cc747074e5c67
Johnny Jeong <johnny.j.jeong@gmail.com>
2026-05-03 22:33:29 -0700

    refactor: finalize repository architecture and visibility

---
+++ Foundations_01_Setup/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Node: zeroSystems2026 Online");
+        System.out.println("Mode: Professional/Full-Structure");
+    }
+}
---
+++ Math_Intuition/Math_Intuition_3Blue1Brown_Linear_Algebra/Chapter_01_Vectors.md
@@ -0,0 +1,4 @@
+# Chapter 01: Vectors
+
+### Visual Intuition:
+Vectors are arrows in space represented by a list of numbers and can be extended or squished by numerical magnitudes called scalars.
